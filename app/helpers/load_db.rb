f0062f228352c14367b8f406c29823de3173cdea
Bruce Woodward <bruce.woodward@gmail.com>
2015-03-03 13:40:57 +1100

    feat(loaddb) process files in downloads directory
    A couple of quicker helper methods to process file names and cope with
    files that may or maynot be compressed.

--- app/helpers/load_db.rb
+++ app/helpers/load_db.rb
@@ -7,4 +7,24 @@ module LoadDB
     Dir.glob("#{dir}/news.*gz").lazy.to_enum
   end
 
+  # If the filename starts with 'news.' and next has a valid date 'YYMMDDMMMM'
+  def self.valid_name filename
+    filename =~ /^news\./ && filename.split(/./)[1] =~ /\d{10}/
+  end
+
+  # If the filename ends with '.gz' open the file via gzip otherwise simply open the file.
+  def self.openfile filename
+    if filename =~ /\.gz$/
+      IO.popen("gunzip < #{filename}")
+    else
+      open(filename)
+    end
+  end
+
+  # Load and parse the file into the database here.
+  def load(dir)
+    files(dir).each do |file|
+    end
+  end
+
 end
--- spec/helpers/load_db_spec.rb
+++ spec/helpers/load_db_spec.rb
@@ -1,7 +1,6 @@
 
 require 'load_db'
 
-
 Fixtures_directory = File.expand_path('test/fixture/load_html/downloads')
 
 describe 'LoadDB.files' do
@@ -11,5 +10,26 @@ describe 'LoadDB.files' do
   end
   it 'should return a list of files matching news.*gz' do
     expect(LoadDB.files(Fixtures_directory).first).to eq Fixtures_directory + '/news.1503031100.gz'
+  end
+end
+
+describe 'LoadDB.valid_name' do
+  it 'should be invalid because of missing prefix' do
+    expect(LoadDB.valid_name('missing.1234567890')).to_not be true
+  end
+  it 'should be invalid because of missing/invalid date' do
+    expect(LoadDB.valid_name('news.150303222')).to_not be true
+  end
+end
+
+describe 'LoadDB.openfile' do
+  it 'should return IO object ready to be read form when the file is compressed' do
+    first_line = LoadDB.openfile(Fixtures_directory + '/dummy.txt').first
+    expect(first_line).to eq "one\n"
+  end
+  it 'should return IO object ready to be read from when the file is not compressed' do
+    io = LoadDB.openfile(Fixtures_directory + '/dummy.txt.gz')
+    expect(io.readline).to eq "one\n"
+    expect(io.readline).to eq "two\n"
   end
 end
