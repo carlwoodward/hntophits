614256f79a061dfc9961c01faae3464ec61d1679
Bruce Woodward <bruce.woodward@gmail.com>
2015-03-03 12:32:02 +1100

    fix(loaddb_spec) promote fixtures directory
    Make the fixture variable global since it's being used all through the
    tests.

--- spec/helpers/load_db_spec.rb
+++ spec/helpers/load_db_spec.rb
@@ -2,13 +2,14 @@
 require 'load_db'
 
 
+Fixtures_directory = File.expand_path('test/fixture/load_html/downloads')
+
 describe 'LoadDB.files' do
-  before(:all) { @fixtures_directory = File.expand_path('test/fixture/load_html/downloads') }
 
   it 'will check that the fixtures directory exists' do
-    expect(Dir.exists?(@fixtures_directory)).to be true 
+    expect(Dir.exists?(Fixtures_directory)).to be true 
   end
   it 'should return a list of files matching news.*gz' do
-    expect(LoadDB.files(@fixtures_directory).first).to eq @fixtures_directory + '/news.1503031100.gz'
+    expect(LoadDB.files(Fixtures_directory).first).to eq Fixtures_directory + '/news.1503031100.gz'
   end
 end
