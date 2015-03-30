f42ccdf1137c85b764b7680da376ba3645ba1a47
Bruce Woodward <bruce.woodward@gmail.com>
2015-03-31 06:28:04 +1100

    feat(story) Add association to Story
    Add has_many association to Story

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,7 @@
 class Story < ActiveRecord::Base
 
+  has_many :top_hits
+
   # Check if a new story has reached #1
   def self.new_story?(hn_id)
     Story.find_by(hn_id: hn_id) == nil ? false : true
