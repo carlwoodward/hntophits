9ff6fab537713bde5975900dbb9406b077bb5416
Bruce Woodward <bruce.woodward@gmail.com>
2015-04-05 15:05:35 +1000

    feat(top_hit) Implement top_hit scope
    Return the latest top_hit from TopHit

--- app/models/top_hit.rb
+++ app/models/top_hit.rb
@@ -1,3 +1,10 @@
 class TopHit < ActiveRecord::Base
   belongs_to :story
+
+  scope :top_hit, -> { order(date_seen: :desc).limit(1).first }
+
+  def self.add(story_id:, date:)
+    TopHit.create(story_id: story_id, date: date)
+  end
+
 end
--- spec/models/top_hit_spec.rb
+++ spec/models/top_hit_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TopHit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "create a story with two top_hit entries and select the most recent top hit" do
+    it "selects the newest top hit" do
+      story = create(:story)
+      create(:top_hit, date_seen: Time.now, story: story)
+      top_hit = create(:top_hit, date_seen: Time.now + 1.minute, story: story)
+      expect(TopHit.top_hit).to eq top_hit
+    end
+  end
 end
