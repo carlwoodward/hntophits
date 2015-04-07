33a86e35fb97f6c03870a61cd7753d3d6468fd3a
Bruce Woodward <bruce.woodward@gmail.com>
2015-04-07 20:02:04 +1000

    feat(top_hit) rework to handle empty db

--- app/models/top_hit.rb
+++ app/models/top_hit.rb
@@ -4,16 +4,23 @@ class TopHit < ActiveRecord::Base
   validates :date_seen, presence: true
   validates :story_id, numericality: { integer_only: true } 
 
-  scope :current_top_hit, -> { order(date_seen: :desc).limit(1).first }
+  def self.current_top_hit
+    # returns nil instead of activerecord::relation
+    order(date_seen: :desc).limit(1).first
+  end
 
   def self.new_story_at_top(story_id)
-    TopHit.current_top_hit.story_id != story_id
+    top_hit = TopHit.current_top_hit
+    (top_hit == nil || top_hit.story_id != story_id) ? true : false
   end
 
   def self.create_new_record(story_id:, date_seen:)
     TopHit.create(story_id: story_id, date_seen: date_seen)
   end
 
+  # deal_with_top_hit story_id, date
+  # story_id is the id from the Story table of the currrently tophit on HN
+  #
   def self.deal_with_top_hit(story_id:, date:)
     if new_story_at_top(story_id)
       create_new_record(story_id: story_id, date_seen: date)
