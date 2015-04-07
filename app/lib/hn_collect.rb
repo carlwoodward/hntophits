83e98e3a0702f2e8865caaeb39426b24213e641e
Bruce Woodward <bruce.woodward@gmail.com>
2015-04-07 20:18:33 +1000

    feat(hn_collect) Access HN new API

---
+++ app/lib/hn_collect.rb
@@ -0,0 +1,87 @@
+# vim: ft=ruby
+
+require 'net/http'
+require 'net/https'
+
+# collect the data from the new HN api
+#
+
+module HNCollect
+  extend self
+
+  @cache = {
+    hn_id: nil,
+    description: nil,
+    href: nil
+  }
+
+  def story_cached?(hn_id)
+    @cache[:hn_id] == hn_id ? true : false
+  end
+
+  def update_cache(hn_id, description, href)
+    @cache[:hn_id] = hn_id
+    @cache[:description] = description
+    @cache[:href] = href
+  end
+
+  def one_minute
+    60
+  end
+
+  def last_top_story
+    return @cache.values_at(:hn_id, :description, :href)
+  end
+
+  def https_get path
+    http = Net::HTTP.new('hacker-news.firebaseio.com', 443)
+    http.use_ssl = true
+    resp, data = http.get(path, nil)
+    JSON.parse(resp.body)
+  end
+
+  def get_top_hit
+    https_get '/v0/topstories.json'
+  end
+
+  def get_story hn_id
+    story = https_get "/v0/item/#{hn_id}.json"
+    hn_id, description, href = story.values_at(*%w(id title url))
+    update_cache(hn_id, description, href)
+    return hn_id, description, href
+  end
+
+  def get_top_hit_details top_hit
+    hn_id, description, href = if story_cached?(top_hit)
+      last_top_story
+    else
+      get_story(top_hit)
+    end
+  end
+
+  # adjusted_delay
+  #
+  # Ensure that the collection of data happens every minute by monitoring the time taken
+  # and waiting less because of it.
+  def adjusted_delay(endtime, starttime)
+    [one_minute - (endtime - starttime).abs, 0].max
+  end
+
+  def run_every_minute
+    loop do
+      start_time = Time.now
+      yield
+      end_time = Time.now
+      sleep adjusted_delay(end_time, start_time)
+    end
+  end
+
+  def run
+    run_every_minute do
+      top_hit = get_top_hit.first
+      hn_id, description, href = get_top_hit_details(top_hit)
+      puts "#{Time.now}: #{hn_id}"
+      HN.process_latest_hn_num_one(hn_id: hn_id, description: description, href: href, date: Time.now)
+    end
+  end
+end
---
+++ spec/app/lib/hn_collect_spec.rb
@@ -0,0 +1,49 @@
+
+require "#{Dir.pwd}/app/lib/hn_collect"
+
+describe HNCollect do
+  context "check the caching" do
+    it "should not find the story cached" do
+      expect(HNCollect.story_cached?(10)).to eq false
+    end
+    it "should find the story cached" do
+      HNCollect.update_cache(121, "testing", "no ref")
+      expect(HNCollect.story_cached?(121)).to eq true
+    end
+    it "should have consistent cache contents" do
+      HNCollect.update_cache(121, "testing", "no ref")
+      hn_id, description, href = HNCollect.last_top_story
+      expect(hn_id).to eq 121
+      expect(description).to eq "testing"
+      expect(href).to eq "no ref"
+    end
+  end
+
+  context "get_top_hit_details" do
+    it "should find the story cached and call last_top_story" do
+      top_hit = 121
+      HNCollect.update_cache(top_hit, "testing", "no ref")
+      expect(HNCollect).to receive(:last_top_story)
+      HNCollect.get_top_hit_details(top_hit)
+    end
+    it "shouldn't find the story cached and call get_story" do
+      top_hit = 121
+      HNCollect.update_cache(10, "testing", "no ref")
+      expect(HNCollect).to receive(:get_story).with(top_hit)
+      HNCollect.get_top_hit_details(top_hit)
+    end
+  end
+
+  context "adjusted_delay" do
+    it "should return 0 if time taken is longer than 60 seconds" do
+      expect(HNCollect.adjusted_delay(100, 200)).to eq 0
+    end
+    it "should cope with endtime being before starttime" do
+      expect(HNCollect.adjusted_delay(200, 100)).to eq 0
+    end
+    it "should return a number less than 60 seconds" do
+      expect(HNCollect.adjusted_delay(100, 103)).to eq 57
+    end
+  end
+
+end
