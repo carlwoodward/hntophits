5e502b69a13d4809136a88ef5a0914847f56bb3b
Bruce Woodward <bruce.woodward@gmail.com>
2015-04-29 19:44:32 +1000

    feat(minutes_to_words) Helper for stories
    Create helper for the Stories views that returns a string representing
    the duration that the story has been at number one.

--- spec/helpers/stories_helper_spec.rb
+++ spec/helpers/stories_helper_spec.rb
@@ -11,5 +11,24 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe StoriesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "deal with less than an hour" do
+    it "should return 0 m" do # handle pathologic case.
+      expect(helper.minutes_to_words(10)).to eq "10m"
+    end
+    it "should return 10 m" do
+      expect(helper.minutes_to_words(10)).to eq "10m"
+    end
+    it "should return 1 h" do
+      expect(helper.minutes_to_words(60)).to eq "1h"
+    end
+    it "should return 1 d" do
+      expect(helper.minutes_to_words(1440)).to eq "1d"
+    end
+    it "should return 2 d" do
+      expect(helper.minutes_to_words(1440*2)).to eq "2d"
+    end
+    it "should return 2 d" do
+      expect(helper.minutes_to_words(1440*2+1)).to eq "2d"
+    end
+  end
 end
