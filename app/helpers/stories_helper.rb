0170cee1e5d6ad64d2486c9197a51c15a268db1b
Bruce Woodward <bruce.woodward@gmail.com>
2015-04-29 19:44:32 +1000

    feat(minutes_to_words) Helper for stories
    Create helper for the Stories views that returns a string representing
    the duration that the story has been at number one.

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,14 @@
 module StoriesHelper
+  extend self
+  def minutes_to_words(minutes)
+    minutes = Integer(minutes)
+    hour, day = 60, 60 * 24
+    if minutes < hour
+      "#{minutes}m"
+    elsif minutes < day
+      "#{minutes/hour}h"
+    else
+      "#{minutes/day}d"
+    end
+  end
 end
