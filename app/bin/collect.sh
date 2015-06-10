2eb26f1aaef7a7e54df866a11e4cf13d64d664fd
Bruce Woodward <bruce.woodward@gmail.com>
2015-06-11 08:20:19 +1000

    feat(collect.sh) updates to monitoring and restart
    Send an email with the collection process dies.
    Restart the collection process when it dies.
    Update the setting of the rails environment.

--- app/bin/collect.sh
+++ app/bin/collect.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
 # Bash script to start a rails runner to begin downloading the Hacker News top stories and updating the database.
-
+#
 # Usage
 # Optional first arg with the environment name (an environment name that rails understands)
 
-set -e # Fail if a variable hasn't been defined
 
-ENVIRONMENT=
-if [[ -z $1 ]]
+# No harm in seeing what is happening.
+set -x
+# Fail if a variable hasn't been defined
+set -e
+
+LogStdout=log/HNCollect.stdout LogStderr=log/HNCollect.stderr
+ENVIRONMENT= Env=
+if [[ ! -z $1 ]]
 then
-    ENVIRONMENT="-e $1"
+    case "$1" in
+        p*) Env=production  ;;
+        d*) Env=development ;;
+        t*) Env=test        ;;
+    esac
+    ENVIRONMENT="-e $Env"
 fi
 
-bundle exec rails runner $ENVIRONMENT 'HNCollect.run'
+echo "Using \"$Env\" environment"
+
+while true
+do
+    bundle exec rails runner $ENVIRONMENT 'HNCollect.run' 2>&1 | tee -a log/HNCollect.log 
+    tail $LogStdout $LogStderr | mailx -t bruce.woodward@gmail.com -s 'HNCollect stopped'
+    sleep 10
+done
