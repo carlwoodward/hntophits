d5605351356a009d33bb39719c67988b795d8615
Bruce Woodward <bruce.woodward@gmail.com>
2015-10-22 19:17:17 +1100

    feat(minutes_to_words): update to output format

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -2,13 +2,22 @@ module StoriesHelper
   extend self
   def minutes_to_words(minutes)
     minutes = Integer(minutes)
-    hour, day = 60, 60 * 24
+    minutes = 1 if minutes == 0
+    hour = 60
+    day = hour * 24
+    month = Integer(day * 30.4)
+    year = Integer(12 * month)
     if minutes < hour
-      "#{minutes}m"
+      [minutes, minutes == 1 ? 'min' : 'mins']
     elsif minutes < day
-      "#{minutes/hour}h"
+      [(minutes/hour).to_i, (minutes/hour).to_i == 1 ? 'hour' : 'hours']
+    elsif minutes < month
+      [(minutes/day).to_i, (minutes/day).to_i == 1 ? 'day' : 'days']
+    elsif minutes < year
+      [(minutes/month).to_i, (minutes/month).to_i == 1 ? 'month' : 'months']
     else
-      "#{minutes/day}d"
+      [(minutes/year).to_i, (minutes/year).to_i == 1 ? 'year' : 'years']
     end
   end
+
 end
