24a790beaf49d1ba50acf612113f61a98ec2f17a
Bruce Woodward <bruce.woodward@gmail.com>
2016-01-16 09:59:07 +1100

    fix: Backwards views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def build_hacker_news_href hn_id
     "https://news.ycombinator.com/item?id=#{hn_id}"
   end
+
+  def time_difference_between_now_and_date_last_num_one_in_minutes date
+    (Time.now - date) / 60
+  end
 end
