149e05ff280e0da51ca5276eecd9fed23947a745
Bruce Woodward <bruce.woodward@gmail.com>
2016-05-15 12:31:02 +1000

    Adding stories controller tests

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe StoriesController, type: :controller do
 
+  describe "GET #index" do
+    it "check top_hits and stories coming through from the controller" do
+      stories = []
+      0.upto(2) do |i|
+        story = create(:story)
+        story.time_at_num_one = 2 - i
+        story.save
+        stories << story
+      end
+      tophits = []
+      tophits << create(:top_hit, date_seen: Time.now - 60, story: stories[0])
+      tophits << create(:top_hit, date_seen: Time.now - 2 * 60, story: stories[2])
+      tophits << create(:top_hit, date_seen: Time.now - 3 * 60, story: stories[1])
+      get :index
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(assigns(:top_hits)).to eq tophits
+      expect(assigns(:stories)).to eq stories
+    end
+  end
+
 end
