a9253bc71a0160fe1f274ae1c022fab9edb7a11a
Bruce Woodward <bruce.woodward@gmail.com>
2016-05-17 11:08:24 +1000

    feat: adding spec for show_stories_controller

--- spec/controllers/show_stories_controller_spec.rb
+++ spec/controllers/show_stories_controller_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe ShowStoriesController, type: :controller do
 
+  describe "GET #show" do
+    it "check status of show for show_stories_controller" do
+      stories = 0.upto(2).map do |i|
+        story = create(:story)
+        story.time_at_num_one = 2 - i
+        story.save
+        story
+      end
+
+      get :show
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(assigns(:stories)).to eq stories
+    end
+  end
+
 end
