2aa2ef1709830b07756344b72791c7c83574d75d
Bruce Woodward <bruce.woodward@gmail.com>
2016-06-22 09:58:44 +1000

    remove code that sends email
    Now that the collector no longer sends email, the code that sends emails
    is not needed and is being removed. The "tools" module will remain for
    the time being.

--- app/lib/hn_tools.rb
+++ app/lib/hn_tools.rb
@@ -3,23 +3,23 @@ module HNTools
 
   extend self
 
-  def hostname
-    `uname -n`.chomp
-  end
+  #def hostname
+  #  `uname -n`.chomp
+  #end
 
-  def email msg, hostname=hostname()
-    if hostname =~ /survivor/
-      ubuntu_email msg
-    else
-      mac_email msg
-    end
-  end
+  #def email msg, hostname=hostname()
+  #  if hostname =~ /survivor/
+  #    ubuntu_email msg
+  #  else
+  #    mac_email msg
+  #  end
+  #end
 
-  def ubuntu_email msg
-    system("mailx -t 'bruce.woodward@gmail.com' -s \"#{msg}\" < /dev/null")
-  end
+  #def ubuntu_email msg
+  #  system("mailx -t 'bruce.woodward@gmail.com' -s \"#{msg}\" < /dev/null")
+  #end
 
-  def mac_email msg
-    system("mailx -s \"#{msg}\" 'bruce.woodward@gmail.com' < /dev/null")
-  end
+  #def mac_email msg
+  #  system("mailx -s \"#{msg}\" 'bruce.woodward@gmail.com' < /dev/null")
+  #end
 end
