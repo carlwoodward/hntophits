4704b40527b67dd9504ba4bde0f9cf0520a9d429
Bruce Woodward <bruce.woodward@gmail.com>
2016-06-22 10:32:02 +1000

    ref: remove duplicate tests for valid_hn_id

--- spec/models/hn_spec.rb
+++ spec/models/hn_spec.rb
@@ -4,12 +4,8 @@ require 'rails_helper'
 RSpec.describe HN, type: :model do
   describe 'HN.valid_hn_id' do
     it 'should be valid' do
-      expect(HN.valid_hn_id(1)).to be true
-      expect(HN.valid_hn_id(2)).to be true
       expect(HN.valid_hn_id(30)).to be true
       expect(HN.valid_hn_id(444)).to be true
-      expect(HN.valid_hn_id(5555)).to be true
-      expect(HN.valid_hn_id(77777)).to be true
     end
     it 'should not be valid' do
       expect(HN.valid_hn_id("fred")).to be false
