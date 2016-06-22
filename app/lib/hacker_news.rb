d19efd0043ea9a37b8c62159b242ae818b0fcf7c
Bruce Woodward <bruce.woodward@gmail.com>
2016-06-23 08:46:23 +1000

    ref: commenting on hacker_news.rb legacy
    The exceptions in hacker_news.rb aren't used in the current code but are
    still part of tests. ATM the tests still exist so the code remains. A
    comment has been added explaining this.

--- app/lib/hacker_news.rb
+++ app/lib/hacker_news.rb
@@ -1,4 +1,9 @@
 
+# The exceptions here are from the original code that scraped news.ycombinator.com
+# and are not currently used.
+#
+# See README.md for an overview of the two halves of tophits.
+
 module HackerNews
 
   class ElementNotFound < RuntimeError
