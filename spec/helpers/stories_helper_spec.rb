2db129881244fbffe6e84ed41dd4187717fdedef
Bruce Woodward <bruce.woodward@gmail.com>
2016-06-25 12:54:07 +1000

    add tests to stories helper

--- spec/helpers/stories_helper_spec.rb
+++ spec/helpers/stories_helper_spec.rb
@@ -1,43 +1,43 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the StoriesHelper. For example:
-#
-# describe StoriesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe StoriesHelper, type: :helper do
-  context "deal with less than an hour" do
-    it "should return 1 min" do 
-      expect(helper.minutes_to_words(0)).to eq '1 min'
-    end
-    it "should return 1 min" do 
-      expect(helper.minutes_to_words(1)).to eq '1 min'
-    end
-    it "should return 10 m" do
-      expect(helper.minutes_to_words(10)).to eq '10 mins'
-    end
-    it "should return 1 h" do
-      expect(helper.minutes_to_words(60)).to eq '1 hour'
-    end
-    it "should return 1 h" do
-      expect(helper.minutes_to_words(70)).to eq '1 hour'
-    end
-    it "should return 2 h" do
-      expect(helper.minutes_to_words(130)).to eq '2 hours'
-    end
-    it "should return 1 d" do
-      expect(helper.minutes_to_words(1440)).to eq '1 day'
-    end
-    it "should return 2 d" do
-      expect(helper.minutes_to_words(1440*2)).to eq '2 days'
-    end
-    it "should return 2 d" do
-      expect(helper.minutes_to_words(1440*2+1)).to eq '2 days'
-    end
+  it "given 0 as an arg, it will return 1 min" do 
+    expect(helper.minutes_to_words(0)).to eq '1 min'
+  end
+  it "will return 1 min" do 
+    expect(helper.minutes_to_words(1)).to eq '1 min'
+  end
+  it "will return 10 m" do
+    expect(helper.minutes_to_words(10)).to eq '10 mins'
+  end
+  it "will return 1 h" do
+    expect(helper.minutes_to_words(60)).to eq '1 hour'
+  end
+  it "will return 1 h" do
+    expect(helper.minutes_to_words(70)).to eq '1 hour'
+  end
+  it "will return 2 h" do
+    expect(helper.minutes_to_words(130)).to eq '2 hours'
+  end
+  it "will return 1 d" do
+    expect(helper.minutes_to_words(1440)).to eq '1 day'
+  end
+  it "will return 2 d" do
+    expect(helper.minutes_to_words(1440*2)).to eq '2 days'
+  end
+  it "will return 2 d" do
+    expect(helper.minutes_to_words(1440*2+1)).to eq '2 days'
+  end
+  it "will return 1 month" do
+    expect(helper.minutes_to_words(1440*30.5+1)).to eq '1 month'
+  end
+  it "will return 2 months" do
+    expect(helper.minutes_to_words(1440*30.5*2+2)).to eq '2 months'
+  end
+  it "will return 1 year" do
+    expect(helper.minutes_to_words(1440*30.5*12+2)).to eq '1 year'
+  end
+  it "will return 2 years" do
+    expect(helper.minutes_to_words(1440*30.5*12*2+2)).to eq '2 years'
   end
 end
