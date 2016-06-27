4b935464c5353d8da488895fea2444debe27fcac
Bruce Woodward <bruce.woodward@gmail.com>
2016-06-27 11:56:15 +1000

    adding test for top_hits controller

--- spec/controllers/top_hits_controller_spec.rb
+++ spec/controllers/top_hits_controller_spec.rb
@@ -3,9 +3,11 @@ require 'rails_helper'
 RSpec.describe TopHitsController, type: :controller do
 
   describe "GET #index" do
-    it "assigns the requested student as @student" do
+    it "exercise the #index" do
+      story = create(:story)
+      top_hit = create(:top_hit, story_id: story.id)
       get :index
-      #expect(assigns(:story)).to eq(story)
+      expect(assigns(:top_hits).first.story_id).to eq story.id
     end
   end
 
