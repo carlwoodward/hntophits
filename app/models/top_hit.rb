1730ce2c23b79e1d688f9b6c67e8bcbadc58759a
Bruce Woodward <bruce.woodward@gmail.com>
2016-06-27 11:59:08 +1000

    ref: remove TopHit.create_new_record

--- app/models/top_hit.rb
+++ app/models/top_hit.rb
@@ -26,16 +26,12 @@ class TopHit < ActiveRecord::Base
     (top_hit == nil || top_hit.story_id != story_id) ? true : false
   end
 
-  def self.create_new_record(story_id:, date_seen:)
-    TopHit.create(story_id: story_id, date_seen: date_seen)
-  end
-
   # deal_with_top_hit story_id, date
   # story_id is the id from the Story table of the currrently tophit on HN
   #
   def self.deal_with_top_hit(story_id:, date:)
     if new_story_at_top(story_id)
-      create_new_record(story_id: story_id, date_seen: date)
+      create(story_id: story_id, date_seen: date)
     end
   end
 
