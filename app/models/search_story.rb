0e17a839e4319e937ed08b9a4447d97cf9e464e7
Bruce Woodward <bruce.woodward@gmail.com>
2016-07-05 07:42:05 +1000

    ref seach_story database queries

--- app/models/search_story.rb
+++ app/models/search_story.rb
@@ -16,23 +16,24 @@ class SearchStory < ActiveType::Object
   end
 
   def self.search_description_order_by_time_at_num_one(search_string, direction=:desc)
-    direction = check_direction(direction)
-    Story.where(search_text(search_string)).order(time_at_num_one: direction).map do |r|
-      new(id: r.id, hn_id: r.hn_id, description: r.description, href: r.href,
-          time_at_num_one: r.time_at_num_one, date_seen: r.top_hit.date_seen.to_time)
-    end
+    run_search_query(search_string, "time_at_num_one", direction)
   end
 
   def self.search_description_order_by_date_seen(search_string, direction=:desc)
-    direction = check_direction(direction)
+    run_search_query(search_string, "date_seen", direction)
+  end
+
+  private
+
+  def self.run_search_query(search_string, order_column_name, order_direction)
+    order_direction = check_direction(order_direction)
     query = <<-SQL
       with
       s as (select id, hn_id, href, description, time_at_num_one from stories where #{search_text(search_string)}),
       t as (select * from (select top_hits.story_id, top_hits.date_seen, rank() over (partition by story_id order by date_seen desc) from top_hits) subquery where rank = 1)
       select s.id, s.hn_id, s.description, s.time_at_num_one, s.href, t.date_seen from s join t on s.id = t.story_id
-      order by t.date_seen
+      order by #{order_column_name} #{order_direction.to_s}
     SQL
-    query += direction.to_s
     connection.exec_query(sanitize_sql(query)).map do |r|
       new(id: r["id"], hn_id: r["hn_id"], description: r["description"],
           time_at_num_one: r["time_at_num_one"], date_seen: r["date_seen"])
@@ -42,8 +43,6 @@ class SearchStory < ActiveType::Object
   def self.check_direction(direction)
     [:desc, :asc].include?(direction) ? direction : :desc
   end
-
-  private
 
   def self.search_text(search_string="nothing")
     "description ~* '\\m#{search_string}\\M'"
